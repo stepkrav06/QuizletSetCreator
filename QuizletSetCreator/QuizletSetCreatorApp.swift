ac5f3997ec21c0b276c5ac3199a3223a54aa6b35
Stepan K <stepankravtsov0402@gmail.com>
2023-02-13 16:10:13 +0300

    Initial Commit

---
+++ QuizletSetCreator/QuizletSetCreatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizletSetCreatorApp.swift
+//  QuizletSetCreator
+//
+//  Created by Степан Кравцов on 13.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct QuizletSetCreatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
